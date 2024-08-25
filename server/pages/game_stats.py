29fdc951ada68c3aa0a81b282e2a97d83be6944e
Ramon Meza <ramon_meza@live.com>
2024-08-24 21:56:41 -0400

    Display stats properly, add filter to stats.

--- server/pages/game_stats.py
+++ server/pages/game_stats.py
@@ -7,12 +7,13 @@ from ..supported_games import SUPPORTED_GAMES
 
 
 def GameStats(steam_api_key: str, steam_id: int, app_id: int):
-    details = SteamAPI.get_app_details(app_id)[str(app_id)]["data"]
-    stats = SteamAPI.ISteamUserStats.GetUserStatsForGame(
+    details: dict = SteamAPI.get_app_details(app_id)[str(app_id)]["data"]
+    stats: dict = SteamAPI.ISteamUserStats.GetUserStatsForGame(
         steam_api_key, steam_id, app_id
     )
+    stats_schema: dict = SteamAPI.ISteamUserStats.GetSchemaForGame(steam_api_key, app_id)
     game_name: str = details["name"]
-
+    
     return (
         Title(f"GameStats: {game_name}"),
         Body(
@@ -38,8 +39,16 @@ def GameStats(steam_api_key: str, steam_id: int, app_id: int):
                     cls="flex flex-cols py-8",
                 ),
                 Div(
-                    Pre(Code(json.dumps(details, indent=4), cls="language-json")),
+                    Input(id="FilterInput", type="text", onkeyup="filterList()", placeholder="Search for a stat...", cls="text-black rounded shadow-md p-2"),
+                    Ul(
+                        *[
+                            Li(f"{next(y['displayName'] if y['displayName'] else y['name'] for y in stats_schema['game']['availableGameStats']['stats'] if y['name'] == x['name'])} = {x["value"]}")
+                            for x in stats["playerstats"]["stats"]
+                        ],
+                        id="StatsList",
+                    ),
                     Pre(Code(json.dumps(stats, indent=4), cls="language-json")),
+                    cls="container mx-auto",
                 ),
                 Div(
                     Div(
@@ -62,6 +71,7 @@ def GameStats(steam_api_key: str, steam_id: int, app_id: int):
                 cls="px-4 flex flex-col min-h-screen",
             ),
             Script(code="hljs.highlightAll();"),
+            Script(src="/public/js/filterList.js"),
             cls="container mx-auto",
         ),
     )
